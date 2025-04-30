7b2478a9b8887101de9908aa9b19b77f33e5a22d
xyz987164 <xyz987164@gmail.com>
2025-04-30 19:33:57 +0900

    fix: user entity 제약조건 해제

--- demo/src/main/java/com/project/demo/app/domain/User.java
+++ demo/src/main/java/com/project/demo/app/domain/User.java
@@ -25,7 +25,7 @@ public class User {
     private String userName;
 
     @Setter
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String userNickName;
 
     private String refreshToken;
